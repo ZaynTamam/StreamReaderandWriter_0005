14600587d8eaf7f785221fb2c5ce723acf7206f1
ZaynTamam <tamam.zain78@gmail.com>
2026-06-01 17:08:07 +0700

    Menambahkan metode ambilProduk untuk mengambil produk dari etalase dengan penanganan pengecualian

--- TugasPraktikum13.cpp
+++ TugasPraktikum13.cpp
@@ -18,3 +18,12 @@ public:
         etalase[1] = "Monitor 3periode 24 Inch";
         etalase[2] = "Keyboard Mechanical OWI";
     }
+
+    string ambilProduk(size_t nomorRak) {
+        try {
+            return etalase.at(nomorRak);
+        } catch (const out_of_range& e) {
+            string pesanError = "Gagal Mengambil Barang : Rak nomor " + to_string(nomorRak) + " kosong atau tidak tersedia!";
+            throw runtime_error(pesanError);
+        }
+    }
