046133303827bcbf83816494c05e5a236b7bbc99
ZaynTamam <tamam.zain78@gmail.com>
2026-06-01 17:07:54 +0700

    Menambahkan kelas TokoElektronik dengan atribut etalase dan namaFile

--- TugasPraktikum13.cpp
+++ TugasPraktikum13.cpp
@@ -4,3 +4,17 @@
 #include <vector>
 #include <fstream>
 #include <stdexcept>
+
+using namespace std;
+
+class TokoElektronik {
+private:
+    array<string, 3> etalase;
+    string namaFile = "DB_GibranJaya.txt";
+
+public:
+    TokoElektronik() {
+        etalase[0] = "Laptop Gaming Solo";
+        etalase[1] = "Monitor 3periode 24 Inch";
+        etalase[2] = "Keyboard Mechanical OWI";
+    }
